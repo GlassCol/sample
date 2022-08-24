c782601a9b14971debd751ca50e6fc5b48eed690
GlassCol <coleglass18@outlook.com>
2022-08-24 13:57:20 -0500

    Initial Commit
    Initial demo

---
+++ src/components/Article.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Article = () => {
+  return (
+    <article>
+        <h1>Option 1</h1>
+        <p>Sit reprehenderit culpa aliqua mollit cupidatat ullamco aliquip quis minim velit occaecat minim id. Magna sit officia et irure tempor voluptate non. Ea officia dolore sint cillum est consectetur sint Lorem voluptate minim ad duis eiusmod. Est cillum fugiat qui fugiat laboris eiusmod dolor aliquip ad minim consectetur cillum. Excepteur laborum labore labore ad proident nostrud consequat commodo. Anim magna duis cillum enim ut do nulla esse do laboris enim. Dolore tempor enim officia tempor amet eu cupidatat culpa commodo duis consequat ipsum.</p>
+        <p>Labore irure consectetur enim est est voluptate sint nulla aliquip commodo reprehenderit labore ad amet. Minim adipisicing adipisicing tempor sunt officia consectetur enim minim cillum dolor est ullamco consequat do. Sit deserunt nulla minim labore do tempor elit aliqua nulla consequat. Et sunt in dolor esse enim. Anim do labore ullamco nisi ea enim culpa commodo ad occaecat.</p>
+    </article>
+  )
+}
+
+export default Article
